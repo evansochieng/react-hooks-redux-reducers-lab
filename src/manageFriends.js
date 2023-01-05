40315a8afbd71dad119655e71cfc5536b8eb5fe1
evans ochieng <evansochieng121@gmail.com>
2023-01-05 12:06:37 +0300

    practice creating reducers

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,16 @@
+// Initial state
+const initialState = {
+  friends: []
+};
+
 export function manageFriends(state, action) {
-  // your code here
+  // update state based on the type of action passed
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+    case "friends/remove":
+      return { friends: state.friends.filter( (friend) => friend.id !== action.payload) };
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,11 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  // update state based on the type of action passed
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
